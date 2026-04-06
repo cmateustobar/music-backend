eee1f20bc4d596566a36791aaae1bf87065c07b0
carlos <cmateust@gmail.com>
2026-04-05 20:04:02 -0500

    fix upload middleware

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,9 +2,9 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = "uploads/";
+// Crear carpeta uploads si no existe
+const uploadDir = "uploads";
 
-// crear carpeta si no existe
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
@@ -14,15 +14,12 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    const uniqueName =
+      Date.now() + "-" + file.originalname.replace(/\s+/g, "_");
+    cb(null, uniqueName);
   },
 });
 
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 50 * 1024 * 1024,
-  },
-});
+const upload = multer({ storage });
 
 export default upload;
